efe13244755e0f2e9d3a17bb44f9bf3c836a0cc4
Gustavo Galote <gusgalot3g@gmail.com>
2023-02-13 10:56:58 -0300

    add Fibonacci using recursion

---
+++ 10-recursion-and-sort/fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+   if (n <= 0) return 0;
+   if (n == 1) return 1;
+   return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+   int n = 20;
+   for (int i = 0; i < n; i++) {
+      cout << fibonacci(i) << ", ";
+   }
+   cout << "that's it!";
+   return EXIT_SUCCESS;
+}
